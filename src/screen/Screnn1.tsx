e7dfd9767a3b5cf406ed6894f8bcb335e693bbae
emilio211204 <joseulcuango32@gmail.com>
2024-08-08 09:57:16 -0500

    Enviando

--- src/screen/Screnn1.tsx
+++ src/screen/Screnn1.tsx
@@ -5,7 +5,7 @@ import { useNavigation } from '@react-navigation/native';
 import { styles } from '../../theme/appTheme';
 
 export const Screnn1 = () => {
-  const navigation = useNavigation();
+  const navigation = useNavigation() as any;
 
   const handleImage1Press = () => {
     navigation.navigate('Imagen1');
